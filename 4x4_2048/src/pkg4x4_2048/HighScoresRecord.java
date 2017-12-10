be381f1723f22d0176f3966fd193612706ae811d
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2017-12-10 12:47:06 +0800

    0

--- 4x4_2048/src/pkg4x4_2048/HighScoresRecord.java
+++ 4x4_2048/src/pkg4x4_2048/HighScoresRecord.java
@@ -13,40 +13,30 @@ public class HighScoresRecord {
             Scanner in = new Scanner(System.in);           
             int TotalScore = 0;
             int confirm;
-            //
-            System.out.println("HIGH SCORES");
-            
+   
+            System.out.println("HIGH SCORES");            
             
             Scanner sc = new Scanner(new File("High_Scores.txt"));
             LineNumberReader rd = new LineNumberReader(new FileReader("High_Scores.txt"));
             
             int size;
             String read = "";
-            while((read = rd.readLine())!= null){
-                
+            while((read = rd.readLine())!= null){                
             }
+            
             int line = rd.getLineNumber();
             size = rd.getLineNumber();
-            //using BufferedReader
-//            BufferedReader bf = new BufferedReader(new FileReader("High_Scores.txt"));
-//            
-//            System.out.println(bf.readLine());
-//            System.out.println(bf.readLine());
-            System.out.println("Number of lines is "+line);//ok
-            System.out.println("Length of array "+size+"\n"); //ok
             int allScores[] = new int[size];
             String Scores[] = new String[size];
            
             for(int x = 0; x < Scores.length; x++){
                 Scores[x] = sc.nextLine();
             }
-            
-            for(int i = 0; i < size; i++){
-                System.out.println(Scores[i]);
-            }
             for(int y = 0; y < size; y++){
-                int test = Integer.parseInt(Scores[y]);
+                String value = Scores[y].replaceAll("[^0-9]", "");
+                allScores[y] = Integer.parseInt(value);
             }
+            //sort scores in ascending orders
             for(int pass = 1; pass < allScores.length; pass++){
                 for(int i = 0; i < allScores.length - pass; i++){
                     if(allScores[i] < allScores[i+1]){
@@ -56,17 +46,26 @@ public class HighScoresRecord {
                     }
                 }
             }
-            for(int i = 0; i < allScores.length; i++){
-                System.out.println(allScores[i]);
-            }
+            int rank = 1; 
+            if(allScores.length<10){            
+                for(int i = 0; i < allScores.length; i++){
+                    System.out.println(rank+". " +allScores[i]);
+                    rank++;
+                }
+            }    
+            else
+                for(int i = 0; i < 10; i++){
+                    System.out.println(rank+". " +allScores[i]);
+                    rank++;
+                }
             
-            
-            System.out.println("\nPress any +ve number to start");
+            //for test only. Takde kena mengena dgn actual task
+            System.out.println("\nPress any positive number to start");
             confirm = in.nextInt();
             System.out.println("");
             
             while(confirm >= 0){
-                System.out.print("POWER   ");
+                System.out.print("SCORE   ");
                 int pow = in.nextInt();
                 if(pow >= 0){
                     TotalScore += pow;
@@ -76,28 +75,17 @@ public class HighScoresRecord {
                     break;
                 }
             }
-            
+            //
            
+            //to print score
             BufferedWriter wr = new BufferedWriter(new FileWriter("High_Scores.txt",true));
-            //wr.append('\n');
             wr.write(String.valueOf(TotalScore));
             wr.newLine();
             wr.close();
             
         }catch (IOException | NumberFormatException e){
-            System.out.println("\nStupid NumberFormatException problem");
+            System.out.println("\nNumberFormatException");
         }
-        //            for(int pass = 1; pass < allScores.length; pass++){
-//                for(int i = 0; i < allScores.length - pass; i++){
-//                    if(allScores[i] < allScores[i+1]){
-//                        int hold = allScores[i];
-//                        allScores[i] = allScores[i+1];
-//                        allScores[i+1] = hold;
-//                    }
-//                }
-//            }
-        
- 
     }
     
 }
