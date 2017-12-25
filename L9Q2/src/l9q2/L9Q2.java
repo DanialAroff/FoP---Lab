7a4f908527dd85fac73361f7012476122a2dc66e
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2017-12-25 14:39:23 +0800

    0

---
+++ L9Q2/src/l9q2/L9Q2.java
@@ -0,0 +1,10 @@
+
+package l9q2;
+
+public class L9Q2 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ L9Q2/src/l9q2/PersonProfile.java
@@ -0,0 +1,17 @@
+
+package l9q2;
+
+public class PersonProfile {
+    
+    private String name;
+    private String gender;
+    private String DoB;  //date of birth
+    
+    public PersonProfile(){}
+    
+    public void display(){
+        //System.out.printf(name);
+        System.out.printf("");
+        System.out.printf("");
+    }
+}
