ad1ef5db344998d4385cb3e4fd940288e1c006ab
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2017-12-25 21:11:02 +0800

    0

--- L9Q3/src/l9q3/PersonProfile.java
+++ L9Q3/src/l9q3/PersonProfile.java
@@ -24,92 +24,11 @@ public class PersonProfile {
         System.out.printf("Name : %s\n",name);
         System.out.printf("Gender : %s\n",gender);
         System.out.printf("Date of birth : %s\n",DoB);
-        System.out.println("Course Name   Course Course                        Session    Semester    Grade");
-//        System.out.printf("");
-//        System.out.printf("");
-//        System.out.printf("");        
-        for(int x = 0; x < 5; x++){
-            //System.out.println(cNames+);
-        }
+        System.out.println("Course Name   Course Course                        Session    Semester    Grade");   
     }
 }
 
-class Student extends PersonProfile {
+class Lecturer extends PersonProfile {
     
-    private String cCodes[];
-    private String cNames[];
-    private int session[];
-    private int semester[];
-    private int mark[];
-    
-    public Student() throws FileNotFoundException, IOException{
-        Scanner in = new Scanner(new FileReader("course.txt"));
-        LineNumberReader liner = new LineNumberReader(new FileReader("course.txt"));
-        
-        String fileName;
-        
-        String l = "";
-        while((l = liner.readLine())!= null){         
-        }
-        int size = liner.getLineNumber()/5;
-        
-        cCodes = new String[size];
-        cNames = new String[size];
-        session = new int[size];
-        semester = new int[size];
-        mark = new int[size];
-        
-        for(int i = 0; i < size; i++){
-            cCodes[i] = in.nextLine();
-            cNames[i] = in.nextLine();
-            session[i] = Integer.parseInt(in.nextLine());
-            semester[i] = Integer.parseInt(in.nextLine());
-            mark[i] = Integer.parseInt(in.nextLine());
-        }
-        
-        
-    }
-    public void displayDetails(){
-        for(int x = 0; x < 5; x++){
-//            System.out.println(cCodes[x]);
-//            System.out.println(cNames[x]);
-//            System.out.println(session[x]);
-//            System.out.println(semester[x]);
-//            System.out.println(mark[x]+"\n");
-            String grade = getGrade(mark[x]);
-            System.out.printf("%-13s %-38s %-12d %-8d %s\n",cCodes[x],cNames[x],session[x],semester[x],grade);
-        }
-    }
-    public String getGrade(int mark){
-        if(mark < 35){
-            return "F";
-        }
-        else if(mark < 44){
-            return "E";
-        }
-        else if(mark < 49){
-            return "D";
-        }
-        else if(mark < 54){
-            return "C";
-        }
-        else if(mark < 59){
-            return "C+";
-        }
-        else if(mark < 64){
-            return "B-";
-        }
-        else if(mark < 69){
-            return "B";
-        }
-        else if(mark < 74){
-            return "B+";
-        }
-        else if(mark < 84){
-            return "A-";
-        }
-        else
-            return "A";
-    }
     
 }
